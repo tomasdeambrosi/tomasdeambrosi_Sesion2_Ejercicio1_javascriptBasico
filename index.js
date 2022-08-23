9e91db777dd8095520a8f97e44d1d25c17c85d29
tomasdeambrosi <97841539+tomasdeambrosi@users.noreply.github.com>
2022-08-22 22:19:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Tomas",
+    30,
+    false,
+    new Date(1992, 20, 0),
+    libro = {
+        titulo: "El silmarrillion",
+        autor: "J. R. R. Tolkien",
+        año: 1977,
+        link: "https://www.amazon.com/-/es/J-R-Tolkien/dp/8445077538"
+    }
+];
